af6e7393b9475c755c4f0aa44a53c6b5c8bc3c4b
Vlad Gorbenko <mojidabckuu.22.06.92@gmail.com>
2016-05-15 16:20:54 +0400

    Add delete manager.

---
+++ ActiveRecord/AR/Relation/Managers/DeleteManager.swift
@@ -0,0 +1,25 @@
+//
+//  DeleteManager.swift
+//  SQLite
+//
+//  Created by Vlad Gorbenko on 5/15/16.
+//
+//
+
+class DeleteManager {
+
+    let model: ActiveRecord
+    
+    init(model: ActiveRecord) {
+        self.model = model
+    }
+    
+    func execute() throws {
+        let klass = self.model.dynamicType
+        let structure = Adapter.current.structure(klass.tableName)
+        if let PK = structure.values.filter({ return $0.PK }).first {
+            try Adapter.current.connection.execute("DELETE FORM \(klass.tableName) WHERE \(PK.name) = \(self.model.attributes[PK.name]!)")
+        }
+    }
+    
+}
