4778e881d36f1c11879f77b502682bb76416d9e4
Sajeev <sajeev@farabi.ae>
2017-08-16 15:59:13 +0400

    Fixed crash in Migrations class(Force unwrapping issue)

--- ActiveRecord/AR/Migration/MigrationsController.swift
+++ ActiveRecord/AR/Migration/MigrationsController.swift
@@ -61,7 +61,7 @@ open class MigrationsController {
     open func migrate() {
         self.migrations.sort(by: { $0.timestamp < $1.timestamp })
         let passed = try! SchemaMigration.all()
-        let difference = Set(self.migrations.map({ $0.id })).subtracting(Set(passed.map({ $0.name })))
+        let difference = Set(self.migrations.map({ $0.id })).subtracting(Set(passed.map({ $0.name ?? ""})))
         let pending = self.migrations.filter({ difference.contains($0.id) })
         self.enabled = false
         for migration in self.migrations.filter({ !pending.map{$0.id}.contains($0.id) }) {
