f0998282315ec2482224aadd3a9193efe1ba40a8
alfred <alfred@farabi.ae>
2018-02-03 15:18:24 +0400

    ARDHI-404 crash fixed

--- ActiveRecord/AR/Migration/MigrationsController.swift
+++ ActiveRecord/AR/Migration/MigrationsController.swift
@@ -24,7 +24,7 @@ open class MigrationsController {
             get { return self.name }
             set { self.name = newValue as! String }
         }
-        var name: String!
+        var name: String?
         required init() {}
         required init?(_ map: Map) {}
         
@@ -61,7 +61,7 @@ open class MigrationsController {
     open func migrate() {
         self.migrations.sort(by: { $0.timestamp < $1.timestamp })
         let passed = try! SchemaMigration.all()
-        let difference = Set(self.migrations.map({ $0.id })).subtracting(Set(passed.map({ $0.name ?? ""})))
+        let difference = Set(self.migrations.map({ $0.id })).subtracting(Set(passed.flatMap({ $0.name ?? ""})))
         let pending = self.migrations.filter({ difference.contains($0.id) })
         self.enabled = false
         for migration in self.migrations.filter({ !pending.map{$0.id}.contains($0.id) }) {
